7c1b8d33c1a647234ab2aad14f04f3d9901782f8
berniemackie97 <berniemack97@icloud.com>
2024-05-28 00:55:22 -0400

    init commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "SpeakSync",
+  description:
+    "SpeakSync is a video conferencing app built with Next.js and Stream",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
